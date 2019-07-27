75a7bc2ad8aa8782b598a27ffb6f13bb5ca56c7e
evanboissonnot <evan@boissonnot.fr>
2019-07-27 08:56:45 +0200

    switch expression c#7

---
+++ TestinInDotNetCore/SwitchExpression/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SwitchExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Wookie> wookies = new List<Wookie>()
+            {
+                new Wookie() { Name = "Chewie", Size = 3.5M },
+                new Wookie() { Name = "Attichitcuk", Size = 3.4M },
+                new Wookie() { Name = "Chak", Size = 3.2M }
+            };
+
+            foreach (var wookie in wookies)
+            {
+                WorkOneItem(wookie);
+            }
+        }
+
+        // C# 7 way
+        static void WorkOneItem(object item)
+        {
+            switch(item)
+            {
+                case Wookie wookie when wookie.Size > 3.4M:
+                    {
+                        Console.WriteLine("C'est un wookie grand");
+                    }
+                    break;
+
+                case Wookie wookie:
+                    {
+                        Console.WriteLine("C'est un wookie");
+                    } break;
+            }
+        }
+    }
+}
---
+++ TestinInDotNetCore/SwitchExpression/Wookie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwitchExpression
+{
+    public class Wookie
+    {
+        #region Properties
+        public string Name { get; set; }
+        public decimal Size { get; set; }
+        #endregion
+    }
+}
